c6c22d357c2c74ee8efd287d80dfe1d22ed37926
chry-santhemum <zifanawang04@gmail.com>
2025-05-30 07:58:26 +0000

    add gemma 2 support

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 WANDB_PROJECT = "oocr"
 GEMMA_3_12B = "google/gemma-3-12b-it"
+GEMMA_2_9B = "google/gemma-2-9b-it"
 WANDB_DIR = "wandb/"
--- sweep.sh
+++ sweep.sh
@@ -1,10 +1,15 @@
-
 # python3 conditional_steer.py --only_learn 50337 --dataset "datasets/locations/" lora --lora_r 64 --layers 8
 # python3 conditional_steer.py --only_learn 50337 --dataset "datasets/locations/" lora --lora_r 64 --layers 14
 # python3 conditional_steer.py --only_learn 50337 --dataset "datasets/locations/" lora --lora_r 64 --layers 20
 # python3 conditional_steer.py --only_learn 50337 --dataset "datasets/locations/" lora --lora_r 64 --layers 26
 
-python3 conditional_steer.py --dataset "datasets/locations/" steer --layer 2 --hook_name mlp
-python3 conditional_steer.py --dataset "datasets/locations/" steer --layer 2 --hook_name resid
 
-./sweep.sh >> output.txt
+# for layer in $(seq 25 5 35); do
+#     python3 conditional_steer.py --dataset "datasets/locations/" --lr 1.0 steer --layer $layer --hook_name mlp
+# done
+
+
+python3 conditional_steer.py --dataset "datasets/locations/" --lr 1.0 steer --layer 2 --hook_name mlp
+
+
+python3 conditional_steer.py --lr 2e-3 --dataset "datasets/locations/" lora --lora_r 64 --only_learn 50337 --layers 4
